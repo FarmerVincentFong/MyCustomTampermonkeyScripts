1eada193b39deb731ef9f37885d9d7651a21aaee
FarmerVincentFong <35392169+FarmerVincentFong@users.noreply.github.com>
2021-06-30 12:06:11 +0800

    修复msdn的页面变动问题1

--- 自修改MSDN文档中英文切换固定按钮.user.js
+++ 自修改MSDN文档中英文切换固定按钮.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         自修改MSDN文档中英文切换固定按钮
 // @namespace    https://github.com/FarmerVincentFong
-// @version      0.1
+// @version      0.2
 // @description  添加个固定的中英文切换固定按钮，方便切换阅读。
 // @author       fwq
 // @match        http*://msdn.microsoft.com/zh-cn/*
 // @match        http*://docs.microsoft.com/zh-cn/*
+// @match        http*://msdn.microsoft.com/en-us/*
+// @match        http*://docs.microsoft.com/en-us/*
 // @grant        none
 // @remark       参考自“MSDN中英切换”
 // ==/UserScript==
@@ -35,18 +37,36 @@
         a.addEventListener('click', function(){
             Scrollposition = getScrollTop();
             url += "?px="+ Scrollposition;
+            //改为在本地存储中存当前滚动位置
+            localStorage.setItem('fwqtest',Scrollposition);
             //获取原生“使用英语阅读”的checkbox，切换它。
-            var toggleLabel=document.querySelector("label[for=language-toggle]");
-            toggleLabel.click();
+//             var toggleLabel=document.querySelector("label[for=language-toggle]");
+//             toggleLabel.click();
+            //获取原生“使用英语阅读”按钮，切换它。
+            var toggleBtn=document.querySelector("#lang-link");
+            toggleBtn.click();
             setTimeout(()=>{
                 var lastIndex = url.lastIndexOf("?");
-                var px = url.substring(lastIndex+4,url.lenght);
+//                 var px = url.substring(lastIndex+4,url.lenght);
+                var px = localStorage.getItem('fwqtest')
+                console.log(px)
                 window.scrollTo(0,px);
-            },50)
+            },500)
         }, false );
         document.body.appendChild(a);
     }
     create_button();
+    //还有点问题，不能清空默认滚动高度。以后再说。
+    function CheckScroll(){
+        var px = localStorage.getItem('fwqtest')
+        if(px){
+            setTimeout(()=>{
+                console.log(px)
+                window.scrollTo(0,px);
+            },500)
+        }
+}
+    CheckScroll();
 })();
 // var lastIndex = window.location.href.lastIndexOf("?");
 // var px = window.location.href.substring(lastIndex+4,window.location.href.lenght);
